bf5f6344b2cbb7c205a6623e310eb836c008fd9c
Ali <alinksht@gmail.com>
2025-05-30 22:14:02 +0330

    refactore some code in user-page-client component

--- app/users/dashboard/_components/UserPageClient.tsx
+++ app/users/dashboard/_components/UserPageClient.tsx
@@ -17,13 +17,8 @@ import SleepLogForm from "./SleepLogForm/SleepLogForm";
 import SleepCard from "./SleepLogForm/SleepCard";
 import fetchSleepLogs from "@/services/fetchSleepLogs";
 import formatDurationHoursMinutes from "@/functions/formatDuration";
-
-interface QuickLink {
-  label: string;
-  href: string;
-  bgColor: string;
-  textColor: string;
-}
+import { QuickLink } from "@/types/QuickLinksTypes";
+import ProgramCard from "./ProgramCard";
 
 export default function DashboardPage() {
   const { profile, loading, error } = useUserProfile();
@@ -139,36 +134,5 @@ export default function DashboardPage() {
         ))}
       </div>
     </div>
-  );
-}
-
-function ProgramCard({
-  label,
-  id,
-  fallbackText,
-  href,
-}: {
-  label: string;
-  id: string | null | undefined;
-  fallbackText: string;
-  href: string;
-}) {
-  if (!id) {
-    return (
-      <div className="bg-gradient-to-r from-pink-400 via-purple-500 to-indigo-600 rounded-xl p-4 text-center">
-        <p className="text-white">{label}</p>
-        <p className="text-lg font-bold text-white">{fallbackText}</p>
-      </div>
-    );
-  }
-
-  return (
-    <Link
-      href={href}
-      className="bg-blue-50 hover:bg-blue-100 rounded-xl p-4 block text-center text-blue-700 font-semibold"
-    >
-      <p>{label}</p>
-      <p className="text-lg font-bold underline">View Program</p>
-    </Link>
   );
 }
